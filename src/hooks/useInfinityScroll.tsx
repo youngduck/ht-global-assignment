bf45f0ce83565c3069999083e8d06a31de231399
youngduck <dudejr5839@gmail.com>
2024-03-18 23:10:52 +0900

    refactor: 무한스크롤 hooks화

---
+++ src/hooks/useInfinityScroll.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+import type { IChart } from "@/types/types";
+const useChartData = (fetchFunction: any, initialPage = 1) => {
+  const [dataSource, setDataSource] = useState<IChart[]>([]);
+  const [page, setPage] = useState(initialPage);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const result = await fetchFunction(page);
+    const { data } = result;
+    setDataSource(data);
+  };
+
+  const fetchMoreData = async () => {
+    setTimeout(async () => {
+      const nextPage = page + 1;
+      const result = await fetchFunction(nextPage);
+      const { data, next } = result;
+
+      if (next === null) {
+        setHasMore(false);
+      }
+      setPage(nextPage);
+      setDataSource((prevData) => prevData.concat(data));
+    }, 2000);
+  };
+
+  return { dataSource, fetchMoreData, hasMore };
+};
+
+export default useChartData;
