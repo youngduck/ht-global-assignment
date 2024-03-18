c6b5a92f201e5b298fb72d0d02e59df2f75cde26
youngduck <dudejr5839@gmail.com>
2024-03-18 15:57:48 +0900

    feat: loading

---
+++ src/component/loading/Loading.tsx
@@ -0,0 +1,11 @@
+import "./loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading-wrapper">
+      <span>Loading...</span>
+    </div>
+  );
+};
+
+export default Loading;
