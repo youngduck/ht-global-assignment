b222f7dc22b3ac042d1e9fa643fc9588e0d8bf58
youngduck <dudejr5839@gmail.com>
2024-03-19 15:15:40 +0900

    feat: charge페이지 아이템추가

--- src/pages/chargepage/Chargepage.tsx
+++ src/pages/chargepage/Chargepage.tsx
@@ -1,5 +1,13 @@
+import "./chargepage.scss";
+import ChargeList from "@/component/charge/chargeList/ChargeList";
+
 const Chargepage = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1 className="chargePage-header">포인트 교환</h1>
+      <ChargeList />
+    </>
+  );
 };
 
 export default Chargepage;
